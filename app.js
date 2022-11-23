8873d8a10f777759eea87b93f787ceaeacc359f5
helloswati.github.com <swatigupta7@outlook.in>
2022-11-23 23:40:59 +0530

    modify js file

--- app.js
+++ app.js
@@ -65,8 +65,9 @@ searchBtn.addEventListener('click', () => {
 
 function w3_open() {
     document.getElementById("mySidebar").style.display = "block";
-}
-
-function w3_close() {
+  }
+  
+  function w3_close() {
     document.getElementById("mySidebar").style.display = "none";
-}
+  }
+  
